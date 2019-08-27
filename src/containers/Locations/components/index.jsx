5f687bc76d7672026a863d54baf1dce86356ce02
Jacob Ramirez <mr.jacob.ramirez@gmail.com>
2019-08-27 00:04:01 -0500

    Adding folder for locations

---
+++ src/containers/Locations/components/index.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+
+const propTypes = {
+};
+
+
+export class Locations extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+
+Locations.propTypes = propTypes;
+
+
+export default Locations;
